255c3e4847000ec99ff56cb8ef06872cb665f427
WebberWu <bird23074035@gmail.com>
2016-05-05 20:21:23 +0800

    改用 hapi 試試看

--- app.js
+++ app.js
@@ -1,58 +1,29 @@
-import express from 'express';
-import nunjucks from 'nunjucks';
+'use strict';
 
-let path = require('path');
-let logger = require('morgan');
+const Hapi = require('hapi');
 
-// let routes = require('./routes/index');
-// let users = require('./routes/users');
+const server = new Hapi.Server();
+server.connection({port: 3000});
 
-let app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'html');
-nunjucks.configure('views', { autoescape: true, express: app });
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-
-app.use('/wbapi', function(err, req, res, next){
-    console.log('99999999999');
-    res.send('123');
-});
-// app.use('/', routes);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  let err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Hello World!');
+    }
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: function (request, reply) {
+        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+    }
 });
 
-module.exports = app;
+server.start((err) => {
+    if(err) {
+        throw err;
+    }
+    console.log('Server runing at:', server.info.uri);
+});
